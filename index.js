1af5c4f156fe4dd6db6c8bd2ef5dd143da2bbdaa
benjafrancis <74379029+benjafrancis@users.noreply.github.com>
2020-11-18 15:22:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const vidPanel = document.querySelector('.main-panel');
 const newDiv = document.createElement('div')
-newDiv.innerHTML = '<iframe src="https://go.arena.im/embed/chat/benj-francis-16706/benj-francis-16706-global" style="border: 0; width: 1px; min-width: 100%; height: 350px; border-radius: 4px; margin-top: 20px; margin-bottom:8px; border: 2px solid #c80e14;"></iframe>'
+newDiv.innerHTML = '<iframe src="https://go.arena.im/embed/chat/benj-francis-16706/benj-francis-16706-global" style="border: 0; width: 1px; min-width: 100%; height: 450px; border-radius: 4px; margin-top: 20px; margin-bottom:8px; border: 2px solid #c80e14;"></iframe>'
 vidPanel.appendChild(newDiv)

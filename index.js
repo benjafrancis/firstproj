41150c13ee3a292494abdd7a55c31cf913f2e03b
benjafrancis <74379029+benjafrancis@users.noreply.github.com>
2020-11-18 17:36:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+function addChat() {
+
 const vidPanel = document.querySelector('.main-panel');
 const newDiv = document.createElement('div')
 newDiv.innerHTML = '<iframe src="https://go.arena.im/embed/chat/benj-francis-16706/benj-francis-16706-global" style="border: 0; width: 1px; min-width: 100%; height: 450px; border-radius: 4px; margin-top: 20px; margin-bottom:8px; border: 2px solid #c80e14;"></iframe>'
@@ -12,3 +14,6 @@ popDiv.remove('svg')
 const newPopDiv = document.createElement('div')
 newPopDiv.innerHTML = '<h1 style="font-size: 1.2em">POP OUT CHAT</h1><img style="height: 24px" src="https://arsnovanyc.com/wp-content/uploads/2020/11/popoutwhite.png">'
 popDiv.appendChild(newPopDiv)
+}
+
+addChat()
